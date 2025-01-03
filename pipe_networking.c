f3931083e7c0a2413806e83ac1fcf71d1b6858ff
Irvin Li <irvinli@Irvins-MacBook-Air.local>
2025-01-03 15:34:23 -0500

    Server handshake

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,12 +11,19 @@
   =========================*/
 int server_setup() {
   int from_client = 0;
-  if (mkfifo("non") == -1) {
-    perror("Could not open");
+  if (mkfifo("non", 0666) == -1) {
+    perror("Could not create WKP");
   }
-  mkfifo(get_pid());
+  printf("WKP created. Waiting for client connection...\n");
 
+  if(from_client = open(WKP, O_RDONLY) == -1) {
+    perror("Could not open WKP for reading");
+    unlink("non");
+    exit(1);
+  }
 
+  printf("Client connected. Removing WKP...\n");
+  unlink("non");
   return from_client;
 }
 
@@ -31,6 +38,29 @@ int server_setup() {
   =========================*/
 int server_handshake(int *to_client) {
   int from_client;
+  printf("Waiting for client to send private pipe name...\n");
+  from_client = open("non", O_RDONLY);
+  if (from_client == -1) {
+    perror("Error opening WKP");
+    exit(1);
+  }
+
+  char ppipe[256];
+  if (read(from_client, ppipe, sizeof(ppipe)) <= 0) {
+    perror("Error reading private pipe name from client");
+    close(from_client);
+    unlink("non"); 
+    exit(1);
+  }
+  printf("Received private pipe name from client: %s\n", ppipe);
+
+  *to_client = open(ppipe, O_WRONLY);
+  if (*to_client == -1) {
+    perror("Error opening client's private pipe");
+    close(from_client);
+    unlink("non");
+    exit(1);
+  }
   return from_client;
 }
 
